147437000f4b2f4e6f5206956d874ea1e1b2fd83
Sidor Krytski <sidkrytski@gmail.com>
2024-01-09 19:30:58 +0300

    less5 js str num bool

---
+++ lesson-05/less.js
@@ -0,0 +1,28 @@
+let str = `Hello`;
+let bool = true;
+let num = 13;
+
+let strAndBoolSum = str + bool;
+let strAndNumSum = str + num;
+let numAndBoolSum = num + bool;
+
+let strAndBoolMult = str * bool;
+let strAndNumMult = str * num;
+let numAndBoolMult = num * bool;
+
+let strAndBoolDiv = str / bool;
+let strAndNumDiv = str / num;
+let numAndBoolDiv = num / bool;
+
+let numFromStr = Number(str);
+let numFromBool = Number(bool);
+
+let strFromNum = String(num);
+let strFromBool = String(bool);
+
+let boolFromNum = Boolean(num);
+let boolFromStr = Boolean(str);
+
+
+
+
